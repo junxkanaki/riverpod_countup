f46fa72b95eccdf31fe9b3a58eec3cd6854263f4
Jun <pjsouognl@gmail.com>
2023-01-31 15:35:35 +0900

    データクラスをStateProviderにする section2/22

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/data/count_data.dart';
 import 'package:riverpod_countup/provider.dart';
 
 void main() {
@@ -42,16 +44,63 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
           children: <Widget>[
             Text(ref.watch(titleProvider)),
             Text(
-              ref.watch(countProvider).toString(),
+              // データクラスであるCountDataをStateProviderで管理し、カウンター値をcountDataProviderを通じて取得
+              ref.watch(countDataProvider).count.toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // CountData countData =
+                    //     ref.read(countDataProvider.notifier).state;
+                    // ref.read(countDataProvider.notifier).state =
+                    //     countData.copyWith(
+                    //         count: countData.count + 1,
+                    //         countUp: countData.countUp + 1);
+                    // stateで現在の値を取得し、それに対して値を変更し、providerのstateを変更する
+                    ref.watch(countDataProvider.notifier).update((state) =>
+                        state.copyWith(
+                            count: state.count + 1,
+                            countUp: state.countUp + 1));
+                  },
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    // CountData countData =
+                    //     ref.read(countDataProvider.notifier).state;
+                    // ref.read(countDataProvider.notifier).state =
+                    //     countData.copyWith(
+                    //         count: countData.count - 1,
+                    //         countDown: countData.countDown + 1);
+                    ref.watch(countDataProvider.notifier).update((state) =>
+                        state.copyWith(
+                            count: state.count - 1,
+                            countDown: state.countDown + 1));
+                  },
+                  child: const Icon(CupertinoIcons.minus),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(ref.watch(countDataProvider).countUp.toString()),
+                Text(ref.watch(countDataProvider).countDown.toString()),
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(countProvider.notifier).state++,
+        onPressed: () {
+          ref.watch(countDataProvider.notifier).state =
+              const CountData(count: 0, countUp: 0, countDown: 0);
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'data/count_data.dart';
+
 final titleProvider = Provider<String>((ref) {
   return 'Riverpod Demo Home Page';
 });
@@ -8,3 +10,6 @@ final messageProvider = Provider<String>((ref) => 'Body message');
 
 // 型を指定することでオブジェクト型などの場合は、補完などが利いて便利なので型をかいた方が良き
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>(
+    (ref) => CountData(count: 0, countUp: 0, countDown: 0));
